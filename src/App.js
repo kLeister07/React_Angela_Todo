9449bb31c406c895367838f6af4878a5fbbef367
Kevin Leister <kevinleister07@gmail.com>
2022-12-02 16:37:23 -0600

    create separate component for input

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React, {useState} from 'react';
-import ToDoItem from './components/ToDoItem';
+import ToDoItem from './ToDoItem';
+import InputArea from './InputArea';
 
 function App() {
-    const [inputText, setInputText] = useState('');
     const [items, setItems] = useState([]);
 
-    function handleChange(event) {
-        const newValue = event.target.value;
-        setInputText(newValue);
-    }
-
-    function addItem() {
+    function addItem(inputText) {
         setItems((prevItems) => {
             return [...prevItems, inputText];
         });
-        setInputText('');
     }
 
     function deleteItem(id) {
-      setItems((prevItems) => {
-        return prevItems.filter(
-          (item, index) => {
-            return index !== id
-           }
-         )
+        setItems((prevItems) => {
+            return prevItems.filter((item, index) => {
+                return index !== id;
+            });
         });
     }
 
@@ -32,12 +24,7 @@ function App() {
             <div className='heading'>
                 <h1>To-Do List</h1>
             </div>
-            <div className='form'>
-                <input onChange={handleChange} type='text' value={inputText} />
-                <button onClick={addItem}>
-                    <span>Add</span>
-                </button>
-            </div>
+            <InputArea onAdd={addItem} />
             <div>
                 <ul>
                     {items.map((todoItem, index) => (
@@ -55,3 +42,61 @@ function App() {
 }
 
 export default App;
+
+// import React, {useState} from 'react';
+// import ToDoItem from './components/ToDoItem';
+
+// function App() {
+//     const [inputText, setInputText] = useState('');
+//     const [items, setItems] = useState([]);
+
+//     function handleChange(event) {
+//         const newValue = event.target.value;
+//         setInputText(newValue);
+//     }
+
+//     function addItem() {
+//         setItems((prevItems) => {
+//             return [...prevItems, inputText];
+//         });
+//         setInputText('');
+//     }
+
+//     function deleteItem(id) {
+//       setItems((prevItems) => {
+//         return prevItems.filter(
+//           (item, index) => {
+//             return index !== id
+//            }
+//          )
+//         });
+//     }
+
+//     return (
+//         <div className='container'>
+//             <div className='heading'>
+//                 <h1>To-Do List</h1>
+//             </div>
+//             <div className='form'>
+//                 <input onChange={handleChange} type='text' value={inputText} />
+//                 <button onClick={addItem}>
+//                     <span>Add</span>
+//                 </button>
+//             </div>
+//             <div>
+//                 <ul>
+//                     {items.map((todoItem, index) => (
+//                         <ToDoItem
+//                             key={index}
+//                             id={index}
+//                             text={todoItem}
+//                             onChecked={deleteItem}
+//                         />
+//                     ))}
+//                 </ul>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default App;
