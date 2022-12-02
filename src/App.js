6e5f630b01cbdbecd22d11ef7243f43d9b3713fc
Kevin Leister <kevinleister07@gmail.com>
2022-12-02 15:01:00 -0600

    add item to list

--- src/App.js
+++ src/App.js
@@ -1,41 +1,57 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
+import ToDoItem from './components/ToDoItem';
 
 function App() {
-  const [inputText, setInputText] = useState("");
-  const [items, setItems] = useState([]);
+    const [inputText, setInputText] = useState('');
+    const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setInputText(newValue);
-  }
+    function handleChange(event) {
+        const newValue = event.target.value;
+        setInputText(newValue);
+    }
 
-  function addItem() {
-    setItems(prevItems => {
-      return [...prevItems, inputText];
-    });
-    setInputText("");
-  }
+    function addItem() {
+        setItems((prevItems) => {
+            return [...prevItems, inputText];
+        });
+        setInputText('');
+    }
 
-  return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-          {items.map(todoItem => (
-            <li>{todoItem}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+    function deleteItem(id) {
+      setItems((prevItems) => {
+        return prevItems.filter(
+          (item, index) => {
+            return index !== id
+           }
+         )
+        });
+    }
+
+    return (
+        <div className='container'>
+            <div className='heading'>
+                <h1>To-Do List</h1>
+            </div>
+            <div className='form'>
+                <input onChange={handleChange} type='text' value={inputText} />
+                <button onClick={addItem}>
+                    <span>Add</span>
+                </button>
+            </div>
+            <div>
+                <ul>
+                    {items.map((todoItem, index) => (
+                        <ToDoItem
+                            key={index}
+                            id={index}
+                            text={todoItem}
+                            onChecked={deleteItem}
+                        />
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
